58427fd49626a17778649b76d240e89683c2e79e
Halvor <stektpotet@gmail.com>
2015-06-03 00:57:12 +0200

    Added LevelGoal-structure

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Level: MonoBehaviour
+{
+	public LevelGoal[]	levelGoals;
+
+	public void setLevelGoals(params LevelGoal[] goals)
+	{
+		List<LevelGoal> lgList = new List<LevelGoal>();
+		foreach (LevelGoal lg in goals)
+		{
+			lgList.Add(lg);
+		}
+		levelGoals = lgList.ToArray();
+	}
+}
---
+++ Assets/Scripts/LevelGoal.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGoal : MonoBehaviour
+{
+	public Vector2 requiredRadius;
+	public Vector2 requiredAge;
+	public Vector2 requiredSpeed;
+	public LevelGoalCondition goalCondition;
+	public GameObject tracking;
+
+
+	bool completed = false;
+	delegate bool GoalCheck( GameObject testing );
+	GoalCheck gc;
+	void Start()
+	{
+		//Setup what to test
+		for(int i=1; i < Mathf.Pow(2, System.Enum.GetNames( typeof(LevelGoal.LevelGoalCondition) ).Length); i+=i )
+		{
+			switch (goalConditionProp.intValue & i) 
+			{
+			case (int)LevelGoal.LevelGoalCondition.AREA:
+				gc += TestArea;
+				break;
+			case (int)LevelGoal.LevelGoalCondition.AGE:
+				gc += TestAge;
+				break;
+			case (int)LevelGoal.LevelGoalCondition.SPEED:
+				gc += TestSpeed;
+				break;
+			default:
+				break;
+			}
+		}
+	}
+
+	void Update()
+	{
+		//Perform tests
+		if(gc != null)
+		{
+			gc(tracking); //itterate through objects?
+		}
+	}
+
+	//within min and max range
+	public bool TestArea(GameObject go)
+	{
+		return (go.transform.position-this.transform.position).magnitude >= this.requiredRadius.x && 
+			(go.transform.position-this.transform.position).magnitude <= this.requiredRadius.y;
+	}
+
+	public bool TestAge(GameObject go)
+	{
+		return (go.transform.position-this.transform.position).magnitude >= this.requiredRadius.x && 
+			(go.transform.position-this.transform.position).magnitude <= this.requiredRadius.y;
+	}
+	public bool TestSpeed(GameObject go)
+	{
+		return (go.transform.position-this.transform.position).magnitude >= this.requiredRadius.x && 
+			(go.transform.position-this.transform.position).magnitude <= this.requiredRadius.y;
+	}
+
+	[System.Flags]
+	public enum LevelGoalCondition
+	{
+		AGE 	= 1,	//0x00000001
+		AREA	= 2,	//0x00000010
+		SPEED	= 4		//0x00000100
+	}
+}
+//AGING CLASS?
