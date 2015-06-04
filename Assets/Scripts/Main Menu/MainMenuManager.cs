182f5bdbd9f4596b04a6db91414e82f93c6358bd
Froff <Fridtjof_Eik@hotmail.com>
2015-06-04 15:27:49 +0200

    Added a main menu under the scene name 'menu.unity'

---
+++ Assets/Scripts/Main Menu/MainMenuManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour
+{
+	public GameObject LevelSelectButton;
+	public GameObject QuitGameButton;
+	public GameObject MainMenuButton;
+
+	private UIScreen currentScreen;
+
+	void Start ()
+	{
+		currentScreen = UIScreen.MainMenu;
+	}
+
+	void Update ()
+	{
+		if (currentScreen == UIScreen.MainMenu)
+		{
+			LevelSelectButton.SetActive(true);
+			QuitGameButton.SetActive(true);
+			MainMenuButton.SetActive(false);
+		}
+
+		else if (currentScreen == UIScreen.LevelSelect)
+		{
+			LevelSelectButton.SetActive(false);
+			QuitGameButton.SetActive(false);
+			MainMenuButton.SetActive(true);
+		}
+	}
+
+	public void LevelSelectButtonClicked ()
+	{
+		currentScreen = UIScreen.LevelSelect;
+	}
+	
+	public void MainMenuButtonClicked ()
+	{
+		currentScreen = UIScreen.MainMenu;
+	}
+
+	public void QuitButtonClicked ()
+	{
+		Application.Quit ();
+	}
+
+	public enum UIScreen
+	{
+		MainMenu,
+		LevelSelect
+	}
+}
