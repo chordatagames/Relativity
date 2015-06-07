06693b1c0a021b083885970c776d5f9eb5e02029
picklemagnet <infinitefacepalm@gmail.com>
2015-06-07 03:23:09 +0200

    the ship rotates! wow.

--- Assets/Scripts/Monobehaviour/Ship.cs
+++ Assets/Scripts/Monobehaviour/Ship.cs
@@ -4,16 +4,27 @@ using System.Collections;
 public class Ship : MonoBehaviour
 {
 	public LevelGoal[] shipGoals;
+	public GameObject sprite;
 
 	bool _completed;
 	public bool completed { get { return _completed; } }
 
+	private Rigidbody2D rigidbody;
+
 	void Start()
 	{
+		rigidbody = GetComponent<Rigidbody2D>();
 		foreach (LevelGoal lg in shipGoals)
 		{
 			lg.tracking = gameObject;
 		}
+	}
+
+	void Update() 
+	{
+		Vector2 fromCenterDir = rigidbody.velocity.normalized;
+		float angle = Vector2.Angle(Vector2.right, -fromCenterDir)*Mathf.Deg2Rad*Mathf.Sign(-fromCenterDir.y) + Mathf.PI/2;
+		transform.rotation = new Quaternion(0, 0, 1 * Mathf.Sin (angle/2), Mathf.Cos (angle/2));
 	}
 
 	public void UpdateGoals()
@@ -35,6 +46,6 @@ public class Ship : MonoBehaviour
 	void ShipComplete()
 	{
 		Debug.Log("Ship " + GetInstanceID() + " completed!");
-		transform.parent.GetComponent<Level>().UpdateLevel(); //TODO Get level
+		GameController.control.level.UpdateLevel(); //TODO Get level
 	}
 }
