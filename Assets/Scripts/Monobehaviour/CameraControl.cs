aeb68728e0a0895f2e4e0342c0b6ff0a8571f78a
picklemagnet <infinitefacepalm@gmail.com>
2015-06-07 18:33:04 +0200

    added zoom, fixed camera drag a little bit.

--- Assets/Scripts/Monobehaviour/CameraControl.cs
+++ Assets/Scripts/Monobehaviour/CameraControl.cs
@@ -1,26 +1,40 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DragCamera : MonoBehaviour {
+public class CameraControl : MonoBehaviour 
+{
 	public float dragRate;
+	public float zoomRate;
 
 	private Vector2 difference;
 	private Vector2 pos;
 	private bool dragging = false;
 
-	void LateUpdate() {
+	void LateUpdate() 
+	{
+		// they're placed in their own functions because they're completely
+		// unrelated, not because they're meant to be reused. zoom is too
+		// simple to place in its own class.
+		Drag();
+		Zoom();
+	}
+	
+	void Drag()
+	{
+		// camera dragging: this is the kinda stuff that tends to be unrebindable,
+		// so i don't see a big problem with just hard coding it like this.
 		if (Input.GetMouseButtonDown(1))
 		{
 			dragging = true;
 			difference = Camera.main.ScreenToViewportPoint(Input.mousePosition);
 			pos = transform.position;
 		}
-
+		
 		if (Input.GetMouseButtonUp(1))
 		{
 			dragging = false;
 		}
-
+		
 		if (Input.GetMouseButton(1))
 		{
 			transform.position = new Vector3
@@ -30,5 +44,11 @@ public class DragCamera : MonoBehaviour {
 				transform.position.z
 			);
 		} 
+	}
+
+	void Zoom()
+	{
+		// same as the above w/r/t hardcoded controls.
+		Camera.main.orthographicSize -= Input.GetAxis("Mouse ScrollWheel") * zoomRate;
 	}
 }
